034f765e0e76e57065ffcc99a55a4ae742fa3405
ShunMatsumoto <mr.bigmr.little@gmail.com>
2020-02-14 15:55:15 +0900

    イベント後の処理

--- js/main.js
+++ js/main.js
@@ -8,5 +8,16 @@ $(function () {
         $headerClone = $header.contents().clone(),
         $headerCloneContainer = $('<div class="page-header-clone"></div>'),
         threshold = $header.offset().top + $header.outerHeight();
+
+    $headerCloneContainer.append($headerClone);  //コンテナにヘッダーのクローンを挿入
+    $headerCloneContainer.appendTo('body');  //コンテナをbodyの最後に挿入
+
+    $window.on('scroll', function () {
+      if ($window.scrollTop() > threshold) {
+        $headerCloneContainer.addClass('visible');
+      } else {
+        $headerCloneContainer.removeClass('visible');
+      }
+    });
   });
 });
